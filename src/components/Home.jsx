db9186cb3df0c49ce4e10a1ff0c48010b6c2f2f8
Mathias Moureaux <mathias.moureaux@gmail.com>
2018-12-20 18:01:11 +0100

    add components community and concept

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'reactstrap';
 
 class Home extends Component {
   constructor(props) {
@@ -9,26 +8,6 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <section id='concept'>
-          <Row>
-            <Col>
-              <h2>Le concept</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare dui quis odio venenatis, at volutpat metus tincidunt. Ut semper, nisl nec fringilla accumsan, erat magna lacinia quam, a aliquet arcu tortor in erat.
-              </p>
-            </Col>
-          </Row>
-        </section>
-        <section id='communauté'>
-          <Row>
-            <Col>
-              <h2>La communauté</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare dui quis odio venenatis, at volutpat metus tincidunt. Ut semper, nisl nec fringilla accumsan, erat magna lacinia quam, a aliquet arcu tortor in erat.
-              </p>
-            </Col>
-          </Row>
-        </section>
       </div>);
   }
 }
