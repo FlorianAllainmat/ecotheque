5ae6fbc9d38994f11b01520592921a004c822257
Mathias Moureaux <mathias.moureaux@gmail.com>
2018-12-20 17:51:57 +0100

    commit section concept et communauté

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {}
   }
-  render() { 
-    return ( 
-    <div>
-
-    </div> );
+  render() {
+    return (
+      <div>
+        <section id='concept'>
+          <Row>
+            <Col>
+              <h2>Le concept</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare dui quis odio venenatis, at volutpat metus tincidunt. Ut semper, nisl nec fringilla accumsan, erat magna lacinia quam, a aliquet arcu tortor in erat.
+              </p>
+            </Col>
+          </Row>
+        </section>
+        <section id='communauté'>
+          <Row>
+            <Col>
+              <h2>La communauté</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare dui quis odio venenatis, at volutpat metus tincidunt. Ut semper, nisl nec fringilla accumsan, erat magna lacinia quam, a aliquet arcu tortor in erat.
+              </p>
+            </Col>
+          </Row>
+        </section>
+      </div>);
   }
 }
- 
+
 export default Home;
