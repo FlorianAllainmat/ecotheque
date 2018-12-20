f58c5fc87d0f22e9926452af8ccfdd68b71a8a86
robertbarakat <robert.barakat@yahoo.it>
2018-12-20 16:06:51 +0100

    Aplha version of homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import { Route, Switch } from 'react-router-dom';
 import Brands from './containers/Brands';
 import NavBar from './NavBar';
