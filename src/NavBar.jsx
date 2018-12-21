1266c71ce18fcfc2fe59de2db0325163c719917c
AllainmatFlorian <allainmat.florian@hotmail.fr>
2018-12-21 10:58:34 +0100

    csster

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import './NavBar.scss';
 import { Link } from 'react-router-dom';
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
-  NavbarBrand,
   Nav,
   NavItem,
 } from 'reactstrap';
@@ -27,27 +24,23 @@ export default class NavBar extends React.Component {
   }
 
   render() {
-    const { isOpen } = this.state;
     return (
       <div className="nav-bar">
         <Navbar className="navbar-div" dark expand="md">
-          <NavbarBrand className="logo" href="/">
+          <NavItem className="logo" tag={Link} to="/">
             <img src={img} alt="" />
-          </NavbarBrand>
-          <NavItem tag={Link} to="/components/" className="join d-md-none d-lg-none">
-              Ecotheque
           </NavItem>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem tag={Link} to="/components/Concept">
-                  Concept
-              </NavItem>
-              <NavItem tag={Link} to="/components/Communauté">
-                  Communauté 
-              </NavItem>
-            </Nav>
-          </Collapse>
+          <Nav className="ml-auto" navbar>
+            <NavItem tag={Link} to="/Concept">
+                Concept
+            </NavItem>
+            <NavItem tag={Link} to="/Communauté">
+                Communauté 
+            </NavItem>
+            <NavItem tag={Link} to="/resultat-list">
+                Catalogue 
+            </NavItem>
+          </Nav>
         </Navbar>
       </div>
     );
