212a954bfe67d967aa7fac482faa4ed119e8ab07
AllainmatFlorian <allainmat.florian@hotmail.fr>
2018-12-21 05:14:36 +0100

    bo gosse

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,14 +6,6 @@ import './Footer.scss';
 const Footer = () => (
   <Container className="Footer" fluid>
     <Row className="mb-0 d-flex justify-content-around left">
-      <Col className="mb-0" lg={{ offset: 1, size: 3 }}>
-        <li>
-          <Link to="/index/contact">Contact Client</Link>
-        </li>
-        <li>
-          <Link to="/index/politique">Politique de confidentialité</Link>
-        </li>
-      </Col>
       <Col className="mb-0">
         <li>
           <Link to="/index/concept">Concept</Link>
